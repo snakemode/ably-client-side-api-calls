05f72907f573cfe76c044f82909bca4b43394333
Glitch (hello-express) <none>
2020-03-29 12:09:14 +0000

    💧👭 Checkpoint ./server.js:7701387/2168 ./package.json:7701387/24 ./views/index.html:7701387/625 ./public/script.js:7701387/1280

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+
+var Ably = require('ably/promises');
+const client = new Ably.Realtime(process.env.ABLY_API_KEY);
+
 const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.get("/", async (request, response) => {
+  response.sendFile(__dirname + '/views/index.html');
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+app.get("/api/createTokenRequest", async (request, response) => {
+  const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'trainagotchi' });
+  response.send(tokenRequestData);
 });
 
-// listen for requests :)
+/*
+async function createTokenRequest(request) {
+  request = request || { clientId: 'trainagotchi' };
+  return new Promise((resolve, reject) => {  
+    client.auth.createTokenRequest(request, function(err, tokenRequest) {    
+        if (err) { reject(err); } else { resolve(tokenRequest); }
+    });
+  });
+}*/
+
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
