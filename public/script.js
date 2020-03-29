aff4b5510d82d887f393ac9fbf80e0ff2d628397
Glitch (hello-express) <none>
2020-03-29 12:32:14 +0000

    ⛈🎨 Checkpoint ./public/script.js:7701387/1694

--- public/script.js
+++ public/script.js
@@ -1,1 +1,22 @@
-console.log("hello world :o");
+/*globals Ably */
+console.log("Oh hi! 🖤");
+
+async function connect() {
+  const ably = new Ably.Realtime({ authUrl: '/api/createTokenRequest' });
+  const channelId = `[product:ably-tfl/tube]tube:northern:940GZZLUKSX:arrivals`;
+  const channel = await ably.channels.get(channelId);
+  await channel.attach();
+  
+  channel.subscribe(msg => {
+    console.log(msg);
+  });
+  
+  //const resultPage = await channel.history(channel, { untilAttach: true, limit: 1 });
+  //console.log("History retrieved for northern line");
+  
+  //const recentMessage = resultPage.items[0] || { data: [] }; 
+  //console.log(recentMessage.data);
+  
+}
+
+connect();
