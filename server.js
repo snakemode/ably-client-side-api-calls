21318e157a479190eff9e79d46d87e609b18bc26
Jo Franchetti <jofranchetti@gmail.com>
2020-07-06 18:58:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require("express");
+const express = require('express');
 const Ably = require('ably/promises');
+require('dotenv').config()
+
 const client = new Ably.Realtime(process.env.ABLY_API_KEY);
 
 const app = express();
 app.get("/", async (request, response) => {
-  response.sendFile(__dirname + '/views/index.html');
+    response.sendFile(__dirname + '/views/index.html');
 });
 
 app.get("/api/createTokenRequest", async (request, response) => {
-  const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'ably-client-side-api-calls-demo' });
-  response.send(tokenRequestData);
+    const tokenRequestData = await client.auth.createTokenRequest({ 
+        clientId: 'ably-client-side-api-calls-demo' 
+    });
+    response.send(tokenRequestData);
 });
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+  
+app.listen(process.env.PORT, () => console.log(`Example app listening at http://localhost:${process.env.PORT}`))
